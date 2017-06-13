3334cff771d6bf759962b40ab6e003c3b458ae53
siahuat0727 <tansiahuat@gmail.com>
2017-06-13 23:42:34 +0800

    really support different size

--- AlphaReverse/src/reversi/ReversiRule.java
+++ AlphaReverse/src/reversi/ReversiRule.java
@@ -120,14 +120,6 @@ public class ReversiRule {
 	private static final int [][] DIRECTION = new int[][]{{1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}};
 	
 	private static boolean move(int dir) {
-//		if (dir == 0 || dir == 6 || dir == 7)
-//			x++;
-//		if (dir == 0 || dir == 1 || dir == 2)
-//			y++;
-//		if (dir == 2 || dir == 3 || dir == 4)
-//			x--;
-//		if (dir == 4 || dir == 5 || dir == 6)
-//			y--;
 		
 		x += DIRECTION[dir][0];
 		y += DIRECTION[dir][1];
@@ -143,29 +135,115 @@ public class ReversiRule {
 	
 	protected static void goToThis(ArrayList<Position> history){
 		Board.startAgain();
-		int historyColor = Board.WHITE;
-		
-//		String str = "history: ";
-		
+		int historyColor = Board.BLACK;
+				
 		for (Position move : history) {
-//			str += move;
 			go(move.getX(), move.getY(), historyColor);
 			historyColor *= -1;
 		}
-//		System.out.println(str);
+//		System.out.print("go to: ");
+//		printHistory(history);
+//		System.out.println("result after go to this : ");
 //		Board.printBoard();
 	}
 
 	protected static void updateWeight() {
-		weight = new int[][] { { 500, -10, 5, 5, 5, 5, -10, 500 }, { -10, -50, 1, 1, 1, 1, -50, -10 },
-				{ 5, 1, 1, 1, 1, 1, 1, 5 }, { 5, 1, 1, 1, 1, 1, 1, 5 }, { 5, 1, 1, 1, 1, 1, 1, 5 },
-				{ 5, 1, 1, 1, 1, 1, 1, 5 }, { -10, -50, 1, 1, 1, 1, -50, -10 }, { 500, -10, 5, 5, 5, 5, -10, 500 }, };
+		updateWeight(0);
+	}
+	
+	
+	protected static void updateWeight(int n) {
+		final int cornerValue = 100;
+		if(n == 0){
+			if(weight != null && weight[0][0] == cornerValue)
+				return;
+			
+			weight = new int[Board.SIZE][Board.SIZE];
+			
+			// edge(some position will be assigned again)
+			for(int i = 0; i < Board.SIZE; ++i){
+				for(int j = 0; j < Board.SIZE; ++j){
+					weight[i][j] = 5;
+				}
+			}
+			
+			// middle
+			for(int i = 1; i < Board.SIZE-1; ++i){
+				for(int j = 1; j < Board.SIZE-1; ++j){
+					weight[i][j] = 1;
+				}
+			}
+			
+			// corner
+			weight[0][0] = weight[0][Board.SIZE-1] = weight[Board.SIZE-1][0] = weight[Board.SIZE-1][Board.SIZE-1] = cornerValue;
+			
+			// around corner
+			weight[0][1] = weight[0][Board.SIZE-2] = weight[1][0] = weight[1][Board.SIZE-1] = weight[Board.SIZE-2][0] = weight[Board.SIZE-2][Board.SIZE-1] = weight[Board.SIZE-1][1] = weight[Board.SIZE-1][Board.SIZE-2] = -10;
+			
+			// corner duijiao(?)
+			weight[1][1] = weight[1][Board.SIZE-2] = weight[Board.SIZE-2][1] = weight[Board.SIZE-2][Board.SIZE-2] = -50;
+				
+		}else{
+			for(int i = 0; i < Board.SIZE; ++i){
+				for(int j = 0; j < Board.SIZE; ++j){
+					weight[i][j] = 1;
+				}
+			}
+		}
+		
+		
+		
+//		for(int i = 0; i < 8; ++i){
+//			for(int j = 0; j < 8; ++j){
+//				System.out.print(weight[i][j]+ "   ");
+//			}
+//			System.out.println("");
+//		}
 	}
 
 	protected static void setMobility() {
-		mobility = new int[][] { { 5, 1, 3, 3, 3, 3, 1, 5 }, { 1, 1, 2, 2, 2, 2, 1, 1 }, { 3, 2, 2, 2, 2, 2, 2, 3 },
-				{ 3, 2, 2, 2, 2, 2, 2, 3 }, { 3, 2, 2, 2, 2, 2, 2, 3 }, { 3, 2, 2, 2, 2, 2, 2, 3 },
-				{ 1, 1, 2, 2, 2, 2, 1, 1 }, { 5, 1, 3, 3, 3, 3, 1, 5 }, };
+		setMobility(0);
+	}
+	
+	protected static void setMobility(int n) {
+		final int cornerValue = 5;
+		if(n == 0){
+			if(mobility != null && mobility[0][0] == cornerValue)
+				return;
+			
+			mobility = new int[Board.SIZE][Board.SIZE];
+			
+			// edge(some position will be assigned again)
+			for(int i = 0; i < Board.SIZE; ++i){
+				for(int j = 0; j < Board.SIZE; ++j){
+					mobility[i][j] = 3;
+				}
+			}
+			
+			// middle
+			for(int i = 1; i < Board.SIZE-1; ++i){
+				for(int j = 1; j < Board.SIZE-1; ++j){
+					mobility[i][j] = 2;
+				}
+			}
+			
+			// corner
+			mobility[0][0] = mobility[0][Board.SIZE-1] = mobility[Board.SIZE-1][0] = mobility[Board.SIZE-1][Board.SIZE-1] = cornerValue;
+			
+			// around corner
+			mobility[0][1] = mobility[0][Board.SIZE-2] = mobility[1][0] = mobility[1][Board.SIZE-1] = mobility[Board.SIZE-2][0] = mobility[Board.SIZE-2][Board.SIZE-1] = mobility[Board.SIZE-1][1] = mobility[Board.SIZE-1][Board.SIZE-2] = 1;
+			
+			// corner duijiao(?)
+			mobility[1][1] = mobility[1][Board.SIZE-2] = mobility[Board.SIZE-2][1] = mobility[Board.SIZE-2][Board.SIZE-2] = 1;
+				
+		}else{
+			for(int i = 0; i < Board.SIZE; ++i){
+				for(int j = 0; j < Board.SIZE; ++j){
+					mobility[i][j] = 0;
+				}
+			}
+		}
+
 	}
 
 	protected static int getWeight(int color) {
@@ -207,6 +285,16 @@ public class ReversiRule {
 			return "white";
 		else
 			return "black";
+	}
+
+	public static void printHistory(){
+		printHistory(Board.history);
+	}
+	
+	public static void printHistory(ArrayList<Position> history){
+		for(Position pos : history)
+			System.out.print(pos+" ");
+		System.out.println("");
 	}
 	
 	protected static int getStep(){
