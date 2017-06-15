5c6aec6db1db016a80c9e625a12117d87d3298f9
siahuat0727 <tansiahuat@gmail.com>
2017-06-15 21:21:33 +0800

    more beautiful 2(?)

--- AlphaReverse/src/reversi/Test.java
+++ AlphaReverse/src/reversi/Test.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void mmain(String[] args) {
 
 		Board.initialize(8);
 
@@ -17,43 +17,43 @@ public class Test {
 		int AI_level_white = 3;
 
 		int noMove = 0; // 连续两次 noMove 是游戏结束的唯一依据
-		
+
 		int AIcolor = Board.WHITE; // can be changed
-		
+
 		int curColor = Board.BLACK;
-		
+
 		boolean AI_vs_AI = true;
 
 		System.out.println(" start game, press enter to continue......");
 		scanner.nextLine();
-		
+
 		while (noMove < 2) {
 
 			if (ReversiRule.canIgo(curColor) == false) {
 				noMove++;
-				System.out.println(ReversiRule.getName(curColor)+" can't move, press enter to continue......");
+				System.out.println(ReversiRule.getName(curColor) + " can't move, press enter to continue......");
 				scanner.nextLine();
 			} else {
 				noMove = 0;
-				System.out.print(ReversiRule.getName(curColor)+" can move at ");
+				System.out.print(ReversiRule.getName(curColor) + " can move at ");
 				ReversiRule.printWhereCanMove();
-				
-				if (!AI_vs_AI && curColor != AIcolor){
-					int dx,dy;
-				System.out.print("Please enter the position to move (ie. x y): ");
+
+				if (!AI_vs_AI && curColor != AIcolor) {
+					int dx, dy;
+					System.out.print("Please enter the position to move (ie. x y): ");
 					dx = scanner.nextInt();
 					dy = scanner.nextInt();
 					ReversiRule.go(dx, dy, curColor);
-				}else
-					AI.go(curColor, curColor == Board.BLACK ? 3 : 2);
-				
-				System.out.print(ReversiRule.getName(curColor)+" move at ");
-				System.out.println(Board.history.get(Board.history.size()-1));
-				//ReversiRule.printHistory();
+				} else
+					AI.go(curColor, curColor == Board.BLACK ? 2 : 2);
+
+				System.out.print(ReversiRule.getName(curColor) + " move at ");
+				System.out.println(Board.history.get(Board.history.size() - 1));
+				// ReversiRule.printHistory();
 				Board.printBoard();
 
 				System.out.println("white " + Board.whiteCount + " vs black " + Board.blackCount);
-				System.out.println(ReversiRule.getName(curColor)+" finished, press enter to continue......");
+				System.out.println(ReversiRule.getName(curColor) + " finished, press enter to continue......");
 				scanner.nextLine();
 			}
 			curColor = -curColor;
